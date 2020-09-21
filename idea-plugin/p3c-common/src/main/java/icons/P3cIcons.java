9f652c5a401851878dd6d056efdb57eaf8ffe06c
yuewei.huan <hyw_mail@163.com>
2020-09-21 13:07:58 +0800

    IDEA 端 测试提交

--- idea-plugin/p3c-common/src/main/java/icons/P3cIcons.java
+++ idea-plugin/p3c-common/src/main/java/icons/P3cIcons.java
@@ -31,6 +31,8 @@ public final class P3cIcons {
 
     public static final Icon ANALYSIS_ACTION = IconLoader.getIcon("/icons/ali-ide-run.png");
 
+    public static final Icon ANALYSIS_ACTION = IconLoader.getIcon("/icons/ali-ide-run.png");
+
     public static final Icon PROJECT_INSPECTION_ON = IconLoader.getIcon("/icons/qiyong.png");
     public static final Icon PROJECT_INSPECTION_OFF = IconLoader.getIcon("/icons/tingyong.png");
     public static final Icon LANGUAGE = IconLoader.getIcon("/icons/language.png");
